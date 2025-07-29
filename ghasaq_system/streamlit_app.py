f7a3c7de0fdf669cf0c6c4d6ab77f41812f04fa6
omarmagdy787 <romarmagdy926@gmail.com>
2025-07-29 15:10:54 +0300

    Update streamlit_app.py

--- ghasaq_system/streamlit_app.py
+++ ghasaq_system/streamlit_app.py
@@ -150,4 +150,5 @@ try:
         st.dataframe(df, use_container_width=True)
     else:
         st.info("لا توجد بيانات حالياً.")
-st.error(f"حدث خطأ أثناء تحميل البيانات: {e}")
+    except Exception as e:
+            st.error(f"❌ خطأ أثناء الحفظ: {e}")
